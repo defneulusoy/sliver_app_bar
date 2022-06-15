fa0dd9c4da8e016d70025c5bedb15a769169276c
defneulusoy <ulusoy.d@northeastern.edu>
2022-06-15 14:48:45 +0300

    morph button

--- lib/picture_cards/secondPic.dart
+++ lib/picture_cards/secondPic.dart
@@ -4,20 +4,23 @@ import 'package:flutter/material.dart';
 class secondPic extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.indigo,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Row(mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text(
-            'how are you?', 
-            style: TextStyle(
-              color: Color.fromARGB(255, 7, 255, 238),
-               fontSize: 30,
-               fontWeight: FontWeight.bold),)])
-      ],
-    ));
+    return GestureDetector(
+      onTap: (){Navigator.pop(context);},
+      child: Scaffold(
+        backgroundColor: Colors.indigo,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text(
+              'how are you?', 
+              style: TextStyle(
+                color: Color.fromARGB(255, 7, 255, 238),
+                 fontSize: 30,
+                 fontWeight: FontWeight.bold),)])
+        ],
+      )),
+    );
   }
 }
--- lib/picture_cards/thirdPic.dart
+++ lib/picture_cards/thirdPic.dart
@@ -3,19 +3,22 @@ import 'package:flutter/material.dart';
 class thirdPic extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.cyan,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Row(mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text(
-            'hello', style: TextStyle(
-              color: Colors.red[300],
-               fontSize: 30,
-               fontWeight: FontWeight.bold),)])
-      ],
-    ));
+    return GestureDetector(
+      onTap: (){Navigator.pop(context);},
+      child: Scaffold(
+        backgroundColor: Colors.cyan,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text(
+              'hello', style: TextStyle(
+                color: Colors.red[300],
+                 fontSize: 30,
+                 fontWeight: FontWeight.bold),)])
+        ],
+      )),
+    );
   }
 }
